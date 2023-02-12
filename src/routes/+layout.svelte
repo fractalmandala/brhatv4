68bc58cae500a42e071872f568dc4ac1326165ae
amritbrhat <i.amrit.p@gmail.com>
2023-02-12 15:44:11 +0530

    smooth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,6 @@ import HeadGeneral from '$lib/components/globals/Header2023.svelte'
 import '$lib/styles/reader.sass'
 import '$lib/styles/textify.css'
 import '$lib/styles/globaltemplate.sass'
-
-
-
-
 </script>
 
 <HeadGeneral></HeadGeneral>
