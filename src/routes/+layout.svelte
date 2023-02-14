56ea94f93200e0b4765e6a208c7a14aec4cabab3
amritbrhat <i.amrit.p@gmail.com>
2023-02-15 01:05:15 +0530

    riddidid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 // @ts-nocheck
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
 import Header from '$lib/components/globals/Header.svelte'
 import Footer from '$lib/components/globals/FooterGlobal.svelte'
 import '$lib/styles/reader.sass'
 import '$lib/styles/globaltemplate.sass'
+inject({ mode: dev ? 'development' : 'production' });
 
 
 </script>
