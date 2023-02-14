d9f9e9420e92ab1f3f68516777d7d50a0c550458
amritbrhat <i.amrit.p@gmail.com>
2023-02-14 11:22:08 +0530

    yppo

--- src/lib/components/globals/Header.svelte
+++ src/lib/components/globals/Header.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-import supabase from '$lib/db'
 import { reveal } from 'svelte-reveal'
-import AllBrands from '$lib/components/globals/AllBrands.svelte'
-import { fade } from 'svelte/transition';
 import { fly } from 'svelte/transition'
 import { quadIn } from 'svelte/easing'
-import { circIn } from 'svelte/easing'
 let isMenu = false;
 let y = 1
 
@@ -32,7 +28,7 @@ function hideMenu() {
 	</div>
 </div>
 {#if isMenu}
-<div class="c-c-c-c fullscreen" in:fly="{{ duration: 400, x: 0, y: -900, opacity: 1, easing: quadIn}}" out:fly="{{ duration: 400, x: 0, y: -900, opacity: 1, easing: quadIn}}">
+<div class="c-c-c-c fullscreen" in:fly="{{ duration: 400, x: 0, y: -900, opacity: 1, easing: quadIn}}" out:fly="{{ duration: 400, x: 0, y: -900, opacity: 1, easing: quadIn}}" on:click={hideMenu} on:keydown={hideMenu}>
 	<div class="r-r-r-r topstrip">
 		<div class="c-c-c-c sec7"><a href="/">
 			<img src="/images/brhatlogos/horizontalwhite.png" alt="finallogo" /></a>
@@ -41,8 +37,100 @@ function hideMenu() {
 			<img src="/images/icons/hamburger-close.png" alt="closemenu"/>
 		</div>
 	</div>
-	<div class="forno">
-	<AllBrands></AllBrands>
+	<div class="desk-nav r-r-r-r row-of-3">
+		<div class="c-c-c-c navcol1 threebox">
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 200 }}><a href="/anveshi">Bṛhat Anveṣī</a></h5>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 50 }}><a href="/anveshi/chapter">Chapters</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 100 }}><a href="/anveshi/diaries">Travel Diaries</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 150 }}><a href="/anveshi/faq">FAQ</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 200 }}><a href="/anveshi/gallery">Gallery</a></p>
+			</div>
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 200 }}><a href="/drashta">Bṛhat Draṣṭā</a></h5>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 50 }}><a href="/drashta/course">Courses</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 100 }}><a href="/drashta/firekeepers">Firekeepers</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 150 }}><a href="/drashta/drashta">Draṣṭās</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 200 }}><a href="/drashta/schools">Schools</a></p>
+			</div>
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 200 }}><a href="/mrdanga">Bṛhadmṛdaṅga</a></h5>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 50 }}><a href="/mrdanga/kala">Kalā</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 100 }}><a href="/mrdanga/rasa">Rasa</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 150 }}><a href="/mrdanga/kavita">Kavitā</a></p>
+			</div>
+		</div>
+		<div class="c-c-c-c navcol1 threebox">
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 200 }}><a href="/mandala">Fractal Maṇḍala</a></h5>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 50 }}><a href="/mandala/essays">Essays</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 100 }}><a href="/mandala/aphorisms">Caturasūtra</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 150 }}><a href="/mandala/synaptic">Synaptic Reconnection</a></p>
+			</div>
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 200 }}><a href="/rta">Ṛta in Design</a></h5>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 50 }}><a href="/rta/phases">Phases</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 100 }}><a href="/rta/principles">Principles</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 150 }}><a href="/rta/schema">Framework</a></p>
+			</div>
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 200 }}><a href="/aryavarta">Scrolls of Āryavarta</a></h5>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 50 }}><a href="/aryavarta/chapter/01">Kaśyapa's Lament</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 100 }}><a href="/aryavarta/chapter/02">Sūta and Sudā</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 150 }}><a href="/aryavarta/gallery">Gallery</a></p>
+			</div>
+		</div>
+		<div class="c-c-c-c navcol1 threebox">
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 200 }}><a href="/about">About</a></h5>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 50 }}><a href="/about/#actions">Actions</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 100 }}><a href="/about/docs/namelogo">Nāmarūpa</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 150 }}><a href="/about/docs/values">Pratijñā</a></p>
+			<p use:reveal={{ transition: "slide", duration: 200, delay: 200 }}><a href="/about/docs">Vision</a></p>			
+			</div>
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 300 }}><a href="/openlibrary">Open Library</a></h5>
+			</div>
+			<div class="c-c-c-c linkbox boxer">
+			<span class="typ1"></span>
+			<span class="typ2"></span>
+			<span class="typ3"></span>
+			<span class="typ4"></span>
+			<h5 use:reveal={{ transition: "slide", duration: 300, delay: 50 }}><a href="/dhiti">Dhīti</a></h5>
+			</div>
+		</div>
 	</div>
 	<div class="c-c-c-c mob-nav">
 		<div class="c-c-c-c section sec0">
@@ -106,6 +194,53 @@ function hideMenu() {
 {/if}
 
 <style>
+.boxer { position: relative; overflow: hidden; transition: all 0.56s var(--cube1); border: 1px solid #373737; box-shadow: 1px 1px 2px #171717, -1px -1px 2px #212121; align-items: flex-start; justify-content: flex-start; cursor: pointer; border-radius: 4px; }
+.boxer span { position: absolute; top: 0; left: 0;}
+.typ1 { border-top: 1px solid #fe4a49;}
+.typ2 { border-right: 1px solid #fe4a49; width: 100%; }
+.typ3 { border-bottom: 1px solid #fe4a49; height: 100%;}
+.typ4 { border-left: 1px solid #fe4a49;}
+.boxer:hover .typ1 { animation: typ1 0.4s var(--cube1) forwards;}
+.boxer:hover .typ2 { animation: typ2 0.54s var(--cube2) forwards;}
+.boxer:hover .typ3 { animation: typ3 0.7s var(--cube3) forwards;}
+.boxer:hover .typ4 { animation: typ4 0.5s var(--cube4) forwards;}
+.boxer:hover { box-shadow: 7px 10px 15px #171717, -8px -8px 16px #212121; animation: redlines 0.7s var(--cube2) 0.5s forwards; }
+
+.boxer a { color: inherit;}
+.boxer h5 { color: white;}
+.boxer p { color: #676767;}
+.boxer a:hover { color: #fe4a49;}
+
+@keyframes typ1 {
+	0% { width: 0%; margin-left: 0; }
+	50% { width: 100%; margin-left: 0;}
+	100% { width: 100%; margin-left: 100%;}
+}
+
+@keyframes typ2 {
+	0% { height: 0; margin-top: 0;}
+	50% { height: 100%; margin-top: 0;}
+	100% { height: 100%; margin-top: 100%;}
+}
+
+@keyframes typ3 {
+	0% { width: 100%; margin-left: 100%;}
+	50% { width: 100%; margin-left: 0;}
+	100% { width: 0; margin-left:0;}
+}
+
+@keyframes typ4 {
+	0% { height: 100%; margin-top: 100%;}
+	50% { height: 100%; margin-top: 0;}
+	100% { height: 0; margin-top: 0;}
+}
+
+@keyframes redlines {
+ 0% { border: 1px solid transparent;}
+100% { border: 1px solid #ffffff;}
+}
+
+
 
 .logo img {
 	cursor: pointer;
@@ -226,6 +361,10 @@ function hideMenu() {
 .mob-nav h5 { color: white;}
 .mob-nav p { color: #878787;}
 
+.linkbox {
+	transform-origin: center center;
+}
+
 @media screen and (min-width: 900px) {
 	.headerbox, .header { height: 72px;}
 	.headerbox { padding: 0 40px;}
@@ -236,7 +375,7 @@ function hideMenu() {
 	.mobilemenu img { width: 40px; height: 40px;}
 	.menu-red { margin-bottom: -40px;}
 	.fullscreen { padding: 16px 0;}
-	.topstrip { justify-content: space-between; height: 72px; border-bottom: 1px solid #373737; align-items: center; padding: 0 5vw;}
+	.topstrip { justify-content: space-between; border-bottom: 1px solid #373737; align-items: center; padding: 0 5vw 16px 5vw;}
 	.socialicons { gap: 32px;}
 	.closebuttonstrip {
 		width: 100%;
@@ -249,8 +388,14 @@ function hideMenu() {
 		height: 24px;
 		width: 24px;
 	}
-	.forno { display: flex;}
+	.desk-nav { display: flex; padding: 0 6vw;}
 	.mob-nav { display: none;}
+	.sec7 img { object-fit: contain; width: 25%; transform-origin: center center; transition: all 0.23s var(--cube3);}
+	.sec7 img:hover { transform: scale(0.9);}
+	.threebox { min-height: 100%; gap: 24px; justify-content: center;}
+	.linkbox { min-height: 28%; padding-left: 16px; padding-top: 8px; padding-bottom: 8px;}
+	.boxer p { margin-bottom: 6px; font-size: 16px;}
+	.boxer h5 { font-size: 20px;}
 }
 
 @media screen and (max-width: 899px) and (min-width: 768px) {
@@ -262,12 +407,13 @@ function hideMenu() {
 	.mobilemenu { width: 48px; height: 100%;}
 	.mobilemenu img { width: 48px; height: 48px;}
 	.menu-red { margin-bottom: -48px;}
-	.forno { display: flex;}
 	.socialicons img {
 		height: 24px;
 		width: 24px;
 	}
 	.mob-nav { display: none;}
+	.desk-nav { display: flex;}
+	.sec7 img { object-fit: contain; width: 50%;}
 }
 
 @media screen and (max-width: 767px) and (min-width: 576px) {
@@ -279,12 +425,13 @@ function hideMenu() {
 	.mobilemenu { width: 48px; height: 100%;}
 	.mobilemenu img { width: 48px; height: 48px;}
 	.menu-red { margin-bottom: -48px;}
-	.forno { display: flex;}
 	.socialicons img {
 		height: 24px;
 		width: 24px;
 	}
 	.mob-nav { display: none;}
+	.desk-nav { display: flex;}
+	.sec7 img { object-fit: contain; width: 50%;}
 }
 
 @media screen and (max-width: 575px) {
@@ -296,7 +443,6 @@ function hideMenu() {
 	.mobilemenu { width: 48px; height: 100%;}
 	.mobilemenu img { width: 48px; height: 48px;}
 	.menu-red { margin-bottom: -48px;}
-	.forno { display: none;}
 	.fullscreen { padding: 16px 4vw;}
 	.topstrip { justify-content: space-between; border-bottom: 1px solid #373737; align-items: center; margin-bottom: 32px;}
 	.socialicons { gap: 32px; width: 70%; padding-top: 32px;}
@@ -315,6 +461,7 @@ function hideMenu() {
 	.mob-nav h5 { font-size: 32px; text-transform: uppercase; font-weight: 700;}
 	.sec7 img { object-fit: contain; width: 50%;}
 	.sec7 { padding: 8px 0 16px 0; width: 70%;}
+	.desk-nav { display: none;}
 }
 
 </style>
