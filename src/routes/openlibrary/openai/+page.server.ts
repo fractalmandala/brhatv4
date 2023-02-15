0ee94d05d2418d5d42c82b39b2b3a5b6e961d90d
amritbrhat <i.amrit.p@gmail.com>
2023-02-15 15:15:27 +0530

    cll

--- src/routes/openlibrary/openai/+page.server.ts
+++ src/routes/openlibrary/openai/+page.server.ts
@@ -1,34 +1,12 @@
-import { Configuration, OpenAIApi } from 'openai';
-import type { Actions } from './$types';
-
-export const actions: Actions = {
-    submit: async ({ request } : any) => {
-        const promptFormData = await request.formData();
-        const textPrompt = promptFormData.get('text-submission') as string;
-        const imagePrompt = promptFormData.get('image-text-submission') as string;
-
-        const configuration = new Configuration({
-            apiKey:'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',
-        });
-
-        const openai = new OpenAIApi(configuration);
-
-        const textResponse = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: textPrompt,
-            temperature: 0,
-            max_tokens: 45,
-        });
-
-        const imageResponse = await openai.createImage({
-            prompt: imagePrompt,
-            n: 3,
-            size: "1024x1024",
-        });
-
-        return {
-            response: textResponse.data.choices[0].text,
-            imageResponse: imageResponse.data.data
-        }
-    }
-}
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: "org-TQnzOuEZQsRJ4DKLIZyOYBzp",
+    apiKey: import.meta.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Say this is a test",
+  max_tokens: 7,
+  temperature: 0,
+});
