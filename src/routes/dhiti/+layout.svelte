08d95c40ae314ed075a56f705e253eb51b523384
amritbrhat <i.amrit.p@gmail.com>
2023-02-18 04:01:27 +0530

    play

--- src/routes/dhiti/+layout.svelte
+++ src/routes/dhiti/+layout.svelte
@@ -1,9 +1,3 @@
-<script>
-import '$lib/styles/animations.css'
-import '$lib/styles/newglobals.sass'
-</script>
-
-
 <slot></slot>
 
 
--- src/routes/dhiti/[posts]/+layout.svelte
+++ src/routes/dhiti/[posts]/+layout.svelte
@@ -1,10 +1,1 @@
-<script>
-import '$lib/styles/componentstyles.css'
-import FlyingPage from '$lib/components/animations/FlyingPage.svelte'
-</script>
-
-
-<FlyingPage>
 <slot></slot>
-</FlyingPage>
-
