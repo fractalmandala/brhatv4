47ada9cd1aad05b5b9c66a8299a133acbdbc1a66
amritbrhat <i.amrit.p@gmail.com>
2023-02-21 23:40:03 +0530

    fonttry

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@ import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
 import '$lib/styles/componentstyles.sass'
 import '$lib/styles/typographyglobal.sass'
+import '$lib/styles/getfonts.css'
 import Footer from '$lib/components/globals/FooterGlobal.svelte'
 inject({ mode: dev ? 'development' : 'production' });
 
