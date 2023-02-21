ca59455c24e12707b4d999165153ac89359c2e29
amritbrhat <i.amrit.p@gmail.com>
2023-02-21 23:48:33 +0530

    fontend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,10 @@
 // @ts-nocheck
 
 import { dev } from '$app/environment';
+import '@fontsource/ibm-plex-sans-condensed'
 import { inject } from '@vercel/analytics';
 import '$lib/styles/componentstyles.sass'
 import '$lib/styles/typographyglobal.sass'
-import '$lib/styles/getfonts.css'
 import Footer from '$lib/components/globals/FooterGlobal.svelte'
 inject({ mode: dev ? 'development' : 'production' });
 
