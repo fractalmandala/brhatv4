351c458031f55b35dc1950d43a3c8cee81e6302a
amritbrhat <i.amrit.p@gmail.com>
2023-02-22 02:44:32 +0530

    more

--- src/lib/utils/openai.ts
+++ src/lib/utils/openai.ts
@@ -1,6 +1,6 @@
 import { Configuration, OpenAIApi } from 'openai'
 const configuration = new Configuration({
-  apiKey: import.meta.env.OPENAI_API_KEY,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
