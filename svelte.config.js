b381e2a52032be0218013b56b187bcb00ec9e9e4
amritbrhat <i.amrit.p@gmail.com>
2023-03-05 22:35:22 +0530

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
