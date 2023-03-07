0b2c4258e9a6fda6478db775ccab4fb7b951e935
amritbrhat <i.amrit.p@gmail.com>
2023-03-07 10:58:12 +0530

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,115 @@
 <script lang="ts">
-	import supabase from '$lib/db';
-	import { reveal } from 'svelte-reveal';
-	import Brands from '$lib/components/globals/AllBrands.svelte'
-	import BrhatUpdates from '$lib/components/pagecomps/BrhatUpdates.svelte'
-	import Accordion from '$lib/components/pagecomps/HomeAccordion.svelte';
-	import VideoBox from '$lib/components/pagecomps/MrdVids.svelte';
-	import BrhadMrdanga from '$lib/components/vidplaylists/BrhadMrdanga.svelte';
-	import HindiKavita from '$lib/components/vidplaylists/HindiKavita.svelte';
-	import IKSVids from '$lib/components/vidplaylists/IKSVids.svelte';
-	import OtherVids from '$lib/components/vidplaylists/OtherVids.svelte';
-	let y:number = 1;
-	let ih: number
-	let oh: number
-	let ow: number
-	let currentList = 'list-1';
+import supabase from '$lib/db';
+import { reveal } from 'svelte-reveal';
+import Brands from '$lib/components/globals/AllBrands.svelte'
+import BrhatUpdates from '$lib/components/pagecomps/BrhatUpdates.svelte'
+import Accordion from '$lib/components/pagecomps/HomeAccordion.svelte';
+import BrhadMrdanga from '$lib/components/vidplaylists/BrhadMrdanga.svelte';
+import HindiKavita from '$lib/components/vidplaylists/HindiKavita.svelte';
+import IKSVids from '$lib/components/vidplaylists/IKSVids.svelte';
+import OtherVids from '$lib/components/vidplaylists/OtherVids.svelte';
+let y:number = 1;
+let ih: number
+let oh: number
+let ow: number
+let currentList = 'list-1';
 
-	function switchList(newList: string) {
-		currentList = newList;
-	}
+function switchList(newList: string) {
+	currentList = newList;
+}
 
-	async function dhiti() {
-		const { data, error } = await supabase
-			.from('brhat-dhiti')
-			.select()
-			.order('id', { ascending: false })
-			.limit(6);
-		if (error) throw new Error(error.message);
-		return data;
-	}
+export async function updateOne(){
+	const { data, error } = await supabase
+	.from('brhat-updates')
+	.select()
+	.limit(3)
+  if (error) throw new Error(error.message)
+	return data
+}
 
-	async function getBooks() {
-		const { data, error } = await supabase
-			.from('brhat-openlibrary')
-			.select()
-			.order('Sno', { ascending: false })
-			.limit(8);
-		if (error) throw new Error(error.message);
-		return data;
-	}
+async function dhiti() {
+	const { data, error } = await supabase
+		.from('brhat-dhiti')
+		.select()
+		.order('id', { ascending: false })
+		.limit(6);
+	if (error) throw new Error(error.message);
+	return data;
+}
+
+async function getBooks() {
+	const { data, error } = await supabase
+		.from('brhat-openlibrary')
+		.select()
+		.order('Sno', { ascending: false })
+		.limit(15);
+	if (error) throw new Error(error.message);
+	return data;
+}
+
+async function getMrdanga() {
+  const { data, error } = await supabase
+  .from('brhat-youtube')
+  .select()
+	.eq('type','mrdanga')
+  .order('id',{ascending: false})
+  .limit(4)
+  if (error) throw new Error(error.message)
+  return data
+}
+
+async function getKavita() {
+  const { data, error } = await supabase
+  .from('brhat-youtube')
+  .select()
+	.eq('type','hindi')
+  .order('id',{ascending: false})
+  .limit(4)
+  if (error) throw new Error(error.message)
+  return data
+}
+
+async function getIKS() {
+  const { data, error } = await supabase
+  .from('brhat-youtube')
+  .select()
+	.eq('type','iks')
+  .order('id',{ascending: false})
+  .limit(4)
+  if (error) throw new Error(error.message)
+  return data
+}
+
+async function getOther() {
+  const { data, error } = await supabase
+  .from('brhat-youtube')
+  .select()
+	.eq('type','sangam')
+  .order('id',{ascending: false})
+  .limit(4)
+  if (error) throw new Error(error.message)
+  return data
+}
 </script>
 
 <svelte:window bind:scrollY={y} bind:innerHeight={ih} bind:outerHeight={oh} bind:outerWidth={ow}/>
 
 
 <Accordion />
-	<div class="pad-a">
-		<div class="boxc x1">
-			<h1>Bṛhat is a <br><span class="soft">Culture Engine</span></h1>
+
+<div class="pad-a">
+	<div class="section-pads ishfull100 container">
+  	<div class="header">
+		<h1>Bṛhat is a <br><span class="soft">Culture Engine</span></h1>
+		</div>
+  	<div class="para">
 			<h6 class="wide75">
 				To power creatives, research and design rooted in the Indian civilizational consciousness. We
 				convert individual, institutional and collective intent into action, across 3 dimensions.
 			</h6>
 		</div>
-
-		<div class="traybox wrap x2">
-			<div class="ww4 card">
+  	<div class="cols4">
+			<div class="ww1 card">
 				<h6 class="strong">
 					Civilization is Culture in Action
 				</h6>
@@ -62,7 +117,7 @@
 					The civilizational moment needs rooting in Dharma - of this there is no doubt. Thus a core part of our work is culture creatives that draw from the deep pool of Dhārmika heritage.
 				</p>
 			</div>
-			<div class="ww4 card">
+			<div class="ww1 card">
 				<h6 class="strong">
 					It Needs Culture-Compatible Policy
 				</h6>
@@ -70,7 +125,7 @@
 					Radical reorientations are needed in education and ecology. To this end, our focus will be on generating policy currency for culture through frameworks, curriculum and more.
 				</p>
 			</div>
-			<div class="ww4 card">
+			<div class="ww1 card">
 				<h6 class="strong">
 					The Work is Inter-Generational
 				</h6>
@@ -78,19 +133,42 @@
 					It needs leadership with cultural-cognition to carry the Agni. This cognition needs to permeate even brand and organisation - essential quarters for the overton window shift.
 				</p>
 			</div>
-			<div class="ww4 endcol">
+			<div class="ww1 endcol">
 				<button class="mainbutton"><a href="/about">Know More</a></button>
 			</div>
 		</div>
+	</div>
 
-		<div class="boxc y-full the-title x10">
-			<h2><span class="soft">New </span>and Recent</h2>
-			<BrhatUpdates></BrhatUpdates>
-		</div>
+<div class="section-pads ishfull100 container-2">
+  <div class="header-2 the-title">
+		<h2><span class="soft">New </span>and Recent</h2>
+	</div>
+  <div class="cols-3">	
+		{#await updateOne()}
+			<small>.</small>
+			{:then data}
+				{#each data as item}
+					<div class="boxc-r3 card soft">
+						<img src={item.image} alt={item.sequence} />
+						<div class="boxc">
+							<h6>{item.heading}</h6>
+							<p>{item.text.slice(0,200)}</p>
+							<button class="cardbutton"><a href={item.link} target="_blank" rel="noreferrer">{item.buttontext}</a></button>
+						</div>
+					</div>
+				{/each}
+			{:catch error}
+			<pre>{error}</pre>
+			{/await}
+	</div>
+</div>
 
-		<div class="the-title y-full x4">
-			<h2>Explore <span class="soft"> Visual Content</span></h2>
-			<h6 class="wide75">
+<div class="section-pads ishfull100 container-3">
+  <div class="header-3 the-title">
+		<h2>Explore <span class="red"> Visual Content</span></h2>
+	</div>
+  <div class="para-1">
+		<h6 class="wide75">
 				Our visual content ranges from explorations of rasa and bhāva, to articulations of an
 				IKS-implementation strategy for modern India. Select playlists below, or visit our <a
 					href="https://youtube.com/@brhat"
@@ -100,56 +178,113 @@
 				>
 			</h6>
 			<button class="mainbutton">
-			<select on:change={(event) => switchList(event?.target?.value ?? 'list-1')}>
+				<select on:change={(event) => switchList(event?.target?.value ?? 'list-1')}>
 				<option class="w500" value="list-1" on:click={() => switchList('list-1')} on:keydown={() => switchList('list-1')}>Bṛhadmṛdaṅga</option>
 				<option class="w500" value="list-2" on:click={() => switchList('list-2')} on:keydown={() => switchList('list-2')}>Hindi Kavitā</option>
 				<option class="w500" value="list-3" on:click={() => switchList('list-3')} on:keydown={() => switchList('list-3')}>IKS</option>
 				<option class="w500" value="list-4" on:click={() => switchList('list-4')} on:keydown={() => switchList('list-4')}>Others</option>
 			</select></button>
-			<div class="traybox wrap x5">
-				{#if currentList === 'list-1'}
-					<BrhadMrdanga />
-				{:else if currentList === 'list-2'}
-					<HindiKavita />
-				{:else if currentList === 'list-3'}
-					<IKSVids />
-				{:else if currentList === 'list-4'}
-					<OtherVids />
-				{/if}			
-			</div>
-		</div>
-			
+	</div>
+  <div class="cols-4-2">
+			{#if currentList === 'list-1'}
+				{#await getMrdanga()}
+				<small>...</small>
+				{:then data}
+				{#each data as item}
+				<div class="box-video card">
+					<iframe
+					class="m-1"
+					width=100%
+					height=80%
+					src="https://www.youtube.com/embed/{item.videoid}"
+					title={item.name}
+					>
+					</iframe>
+					<p><a href="https://www.youtube.com/watch?v={item.videoid}" target="_blank" rel="noreferrer">{@html item.name}</a></p>
+				</div>
+				{/each}
+				{:catch error}
+				<pre>{error}</pre>
+				{/await}
+			{:else if currentList === 'list-2'}
+				{#await getKavita()}
+				<small>...</small>
+				{:then data}
+				{#each data as item}
+				<div class="box-video card">
+					<iframe
+					class="m-1"
+					width=100%
+					height=80%
+					src="https://www.youtube.com/embed/{item.videoid}"
+					title={item.name}
+					>
+					</iframe>
+					<p><a href="https://www.youtube.com/watch?v={item.videoid}" target="_blank" rel="noreferrer">{@html item.name}</a></p>
+				</div>
+				{/each}
+				{:catch error}
+				<pre>{error}</pre>
+				{/await}					
+			{:else if currentList === 'list-3'}
+				{#await getIKS()}
+				<small>...</small>
+				{:then data}
+				{#each data as item}
+				<div class="box-video card">
+					<iframe
+					class="m-1"
+					width=100%
+					height=80%
+					src="https://www.youtube.com/embed/{item.videoid}"
+					title={item.name}
+					>
+					</iframe>
+					<p><a href="https://www.youtube.com/watch?v={item.videoid}" target="_blank" rel="noreferrer">{@html item.name}</a></p>
+				</div>
+				{/each}
+				{:catch error}
+				<pre>{error}</pre>
+				{/await}
+			{:else if currentList === 'list-4'}
+				{#await getOther()}
+				<small>...</small>
+				{:then data}
+				{#each data as item}
+				<div class="box-video card">
+					<iframe
+					class="m-1"
+					width=100%
+					height=80%
+					src="https://www.youtube.com/embed/{item.videoid}"
+					title={item.name}
+					>
+					</iframe>
+					<p><a href="https://www.youtube.com/watch?v={item.videoid}" target="_blank" rel="noreferrer">{@html item.name}</a></p>
+				</div>
+				{/each}
+				{:catch error}
+				<pre>{error}</pre>
+				{/await}
+			{/if}		
+	</div>
+</div>
 
-
-		<div class="the-title x6">
+<div class="section-pads container-4">
+  <div class="header-4 the-title">
 			<h2>Essays at <span class="soft"> Dhīti</span></h2>
-		</div>
-
-		<div class="traybox allwrap x7">
+	</div>
+  <div class="cols-6">
 			{#await dhiti()}
 			<small>...</small>
 			{:then data}
-				{#each data as item, i}
-					<div class="ww3 card pad blogbox">
+				{#each data as item}
+					<div class="boxc card">
 						<img
 							src={item.image}
-							alt={item.title}
-							use:reveal={{
-								transition: 'slide',
-								delay: 50 * i,
-								duration: 50,
-								easing: 'easeOutCirc'
-							}}
-						/>
+							alt={item.title}/>
 							<cite class="str">{item.category}<br>{item.tags}</cite>
-							<h6 class="w600"
-							use:reveal={{
-								transition: 'slide',
-								delay: 50 * i,
-								duration: 50,
-								easing: 'easeOutCirc'
-							}}
-						>
+							<h6 class="w600">
 							<a href={item.link}>{item.title}</a>
 						</h6>
 						<p>{item.excerpt.slice(0, 200)}...<a href={item.link} class="readmore">Read More</a></p>
@@ -159,90 +294,49 @@
 			{:catch error}
 			<pre>{error}</pre>
 			{/await}
-		</div>
+	</div>
+</div>
 
-		<div class="the-title x8">
-			<h2>Bṛhat <span class="soft"> Open</span> Library</h2>
-			<h6 class="wide75">
-				An online repository of books, papers, texts and scriptures, made available under CC0 1.0
-				License. Gathering point for digitized scripture, Aryan Invasion/Migration, civilizational
-				literature and more.
-			</h6>
-		</div>
-
-		<div class="traybox allwrap x9">
+<div class="section-pads container-5">
+  <div class="the-title header-5">
+		<h2>Bṛhat <span class="soft"> Open</span> Library</h2>
+	</div>
+  <div class="cols-15">
 			{#await getBooks()}
 			<small>...</small>
 			{:then data}
 					{#each data as item, i}
 						<div
-							class="ww4 book card">
+							class="ww1 book card">
 							<h6><a href="/openlibrary/books/{item.slug}">{item.Text}</a></h6>
 							<p>{item.author}</p>
 						</div>
 					{/each}
 			{:catch error}
 			<pre>{error}</pre>
-			{/await}			
-		</div>
+			{/await}	
 	</div>
-
-{#if ow>767}
-<div class="the-title x100">
-	<h2><span class="red">Bṛhat </span><span class="white">Projects</span></h2>
-<Brands></Brands>
 </div>
-{/if}
-
+</div>
 
 <style>
 
-.x100 {
-	background: var(--beau);
-	margin-bottom: 0;
-	padding-top: 16px;
-	padding-bottom: 32px;
-}
 
-.x100 h2 {
-	padding-left: 64px;
-	padding-top: 16px;
-}
+.header { align-items: center; }
 
-.x2 .card h6 { color: var(--strong);}
+.para { align-items: center; }
 
-.blogbox a:hover {
-	color: var(--soft);
-}
-
-
-@keyframes swiped {
-	0% {
-		width: 100%;
+.container-5 { padding-bottom: 64px;}
+@media screen and (max-width: 899px) and (min-width: 576px) {
+	.cols-3 .boxc-r3 img {
+		object-fit: cover;
+		width: 216px;
+		height: 216px;
 	}
-	100% {
-		width: 0;
-	}
-}
-
-
-
-.book {
-	border-bottom: 1px solid #e7e7e7;
-}
-
-@media screen and (min-width: 768px) {
-	.endcol {
-		align-items: center;
-	}
-}
-
-@media screen and (max-width: 767px) and (min-width: 576px) {
-	.x9 { margin-bottom: 64px;}
+	.cols-3 .boxc-r3 { gap: 32px; text-align: left; }
+	.cols-3 .boxc-r3 .boxc { align-items: flex-start; justify-content: flex-start;}
+	.cols-3 .boxc-r3 .boxc .cardbutton { width: 40%; margin-right: 60%;}
 
 }
 
-@media screen and (max-width: 575px) {
-	.x9 { margin-bottom: 64px;}
-}
 </style>
